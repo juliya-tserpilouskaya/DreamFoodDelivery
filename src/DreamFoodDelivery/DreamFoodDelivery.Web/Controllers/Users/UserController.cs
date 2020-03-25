9eb608eaa53ba42cd5a02e4a0a04a9292c6c45d6
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2020-03-25 06:50:49 +0300

    * User controller implemented [Initial approximation. Will be changed] * User Interface Updated [Initial approximation. Will be changed]

--- src/DreamFoodDelivery/DreamFoodDelivery.Web/Controllers/Users/UserController.cs
+++ src/DreamFoodDelivery/DreamFoodDelivery.Web/Controllers/Users/UserController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DreamFoodDelivery.Domain.Logic.InterfaceServices.Users;
+using DreamFoodDelivery.Domain.Users;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace DreamFoodDelivery.Web.Controllers.Users
 {
@@ -16,6 +18,120 @@ namespace DreamFoodDelivery.Web.Controllers.Users
         public UserController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        /// <summary>
+        /// Get all users
+        /// </summary>
+        /// <returns>Returns all users stored</returns>
+        [HttpGet, Route("")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "There are no users in list")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Users were found", typeof(IEnumerable<User>))]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _userService.GetAllAsync();
+            return result == null ? NotFound() : (IActionResult)Ok(result);
+        }
+
+        /// <summary>
+        /// Create new account
+        /// </summary>
+        /// <param name="user">user</param>
+        /// <returns></returns>
+        [HttpPost, Route("")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User added")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Ivalid user data")]
+        public async Task<IActionResult> CreateAsync([FromBody] User user)
+        {
+            if (user is null /*|| ModelState.IsValid*/) 
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userService.CreateAccountAsync(user); 
+            return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.Data);
+        }
+
+        /// <summary>
+        /// Get user account by Id
+        /// </summary>
+        /// <param name="id">user id</param>
+        /// <returns></returns>
+        [HttpGet, Route("{id}")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Ivalid user id")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User doesn't exists")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User was found", typeof(User))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Something goes wrong")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var result = await _userService.GetByIdAsync(id);
+                return result == null ? NotFound() : (IActionResult)Ok(result);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Update the user account
+        /// </summary>
+        /// <param name="user">user</param>
+        /// <returns></returns>
+        [HttpPut, Route("")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid paramater format")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User doesn't exists")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User updated", typeof(User))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Something wrong")]
+        public async Task<IActionResult> Update([FromBody]User user)
+        {
+
+            if (user is null /*|| !ModelState.IsValid*/)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _userService.UpdateAsync(user);
+                return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.Data);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Remove user
+        /// </summary>
+        /// <param name="id">user id</param>
+        /// <returns></returns>
+        [HttpDelete, Route("{id}")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Ivalid ID")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User doesn't exists")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User deleted")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Something goes wrong")]
+        public async Task<IActionResult> RemoveById(string id)
+        {
+            if (!Guid.TryParse(id, out var _) /*|| _orderService.GetById(id) == null*/ /*|| _commentService.GetById(id).UserId != UserId*/)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var result = await _userService.RemoveByIdAsync(id);
+                return result.IsError ? BadRequest(result.Message) : (IActionResult)Ok(result.IsSuccess);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
