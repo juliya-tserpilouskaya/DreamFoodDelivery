d518ef18b88280e17c6cbd0d7d6c77715c668240
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2020-03-25 06:14:47 +0300

    + Dish controller implemented * Dish interface updated

--- src/DreamFoodDelivery/DreamFoodDelivery.Domain.Logic/InterfaceServices/Basket/IDishService.cs
+++ src/DreamFoodDelivery/DreamFoodDelivery.Domain.Logic/InterfaceServices/Basket/IDishService.cs
@@ -1,37 +1,38 @@
-﻿using System;
+﻿using DreamFoodDelivery.Domain.Menu;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DreamFoodDelivery.Domain.Logic.InterfaceServices.Basket
 {
-    class IDishService
+    public interface IDishService
     {
         /// <summary>
         /// Add dish to basket
         /// </summary>
-        /// <param name="dish">New dish</param>
+        /// <param name="id">dish id</param>
         /// <returns></returns>
-        Task<Result<BasketModel>> AddDishAsync(Dish dish);
+        Task<Result<Dish>> AddDishAsync(string id);
 
         /// <summary>
         /// Removes dish from basket
         /// </summary>
         /// <param name="id">dish id</param>
         /// <returns></returns>
-        void RemoveAllByUserId(string id);
+        Task<Result> RemoveByIdAsync(string id);
 
         /// <summary>
         /// Get dishes from user basket
         /// </summary>
-        /// <param name="id">dish id</param>
         /// <returns></returns>
-        void RemoveAllByUserId(string id);
+        Task<IEnumerable<Dish>> GetAllAsync();
 
         /// <summary>
         /// View dish details
         /// </summary>
         /// <param name="id">dish id</param>
         /// <returns></returns>
-        void RemoveAllByUserId(string id);
+        Task<Dish> GetByIdAsync(string id);
     }
 }
